165fcfe5077b4d90ec75724e4fe7799e5df61bb5
Steven Paul Jobs <stevenpauljobs@Toms-HW-Book.local>
2025-04-02 11:02:03 +0800

    dev: reader加个key

--- app/book/[[...slug]]/Read/showBook.tsx
+++ app/book/[[...slug]]/Read/showBook.tsx
@@ -7,15 +7,15 @@ import Mind from './mind'
 const ShowBook = async ({params}: {params: Promise<{slug?: string[]}>}) => {
     const {slug = []} = await params
 
-    const {fileType} = handleSlug(slug)
+    const {fileType, fileUrl} = handleSlug(slug)
 
     if (!fileType) {
         return <div>还未选择文件</div>
     }
 
-    if (fileType === 'pdf') return <PDFReader Header={Header} />
+    if (fileType === 'pdf') return <PDFReader Header={Header} key={fileUrl} />
 
-    if (fileType === 'epub') return <EpubReader Header={Header} />
+    if (fileType === 'epub') return <EpubReader Header={Header} key={fileUrl} />
 
     // 思维导图还有组件问题，@xyflow/react 可能会显示错误，乱连线，@ant-design/graphs可能不支持react19，报错
     if (fileType === 'json') return <Mind />
